62f40c11edceadb849255ded4a01b197bcb40e30
Luke Martin <ljmart02@gmail.com>
2020-12-18 11:23:37 -0700

    Updated so it pushes to an unformatted Readme

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
 
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the name of project?',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Please describe your project.',
+            name: 'describtion',
+        },
+        {
+            type: 'input',
+            message: 'What was the motivation behind creating this repo?',
+            name: 'motivation',
+        },
+        {
+            type: 'input',
+            message: 'What does the user need to know about the repo?',
+            name: 'needtoknow',
+        },
+        {
+            type: 'input',
+            message: 'What does the user need to know about contributing to the rep?',
+            name: 'contribute',
+        },
+        {
+            type: 'input',
+            message: 'Who were the contributers that created the repo?',
+            name: 'partners',
+        },
+        {
+            type: 'input',
+            message: 'Please enter your contact information',
+            name: 'contact',
+        },
+
+
+    ])
+
+    .then((data) => 
+    fs.writeFile('README.md', (`${data.name}
+    ${data.describtion} 
+    ${data.motivation} 
+    ${data.needtoknow}
+    ${data.contribute}
+    ${data.partners} 
+    ${data.contact} 
+    `),
+    
+    (err) =>
+    err ? console.error(err) : console.log('Success!')
+    ));
+
